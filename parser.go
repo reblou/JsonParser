3423d34d7b32db1a5e76b70e5b4db59cba3a3309
Rebecca <rebeayto@gmail.com>
2025-05-13 21:14:39 +0100

    Basic bracket + string pairing

--- parser.go
+++ parser.go
@@ -10,17 +10,44 @@ const (
 	StringState
 )
 
+var strategy func(*stack.Stack, byte)
+
 func isValidJson(s string) bool {
 	// Build stack of states once one state concluded, pop off stack
 	state := stack.New()
+	strategy = NormalCharParse
 	for i := 0; i < len(s); i++ {
-		switch s[i] {
-		case '{':
-			state.Push(ObjectState)
-		case '[':
-			state.Push(ArrayState)
-		case '"':
-			state.Push(StringState)
+		strategy(state, s[i])
+	}
+
+	if state.Len() > 0 {
+		panic("Incomplete Json")
+	}
+	return true
+}
+
+func NormalCharParse(state *stack.Stack, c byte) {
+	switch c {
+	case '{':
+		state.Push(ObjectState)
+	case '[':
+		state.Push(ArrayState)
+	case '"':
+		strategy = StringCharParse
+	case '}':
+		if l := state.Pop(); l != ObjectState {
+			panic("Invalid Json, unexpected }")
 		}
+	case ']':
+		if l := state.Pop(); l != ArrayState {
+			panic("Invalid Json, unexpected ]")
+		}
+	}
+}
+
+func StringCharParse(state *stack.Stack, c byte) {
+	if c == '"' {
+		// end string parse
+		strategy = NormalCharParse
 	}
 }
